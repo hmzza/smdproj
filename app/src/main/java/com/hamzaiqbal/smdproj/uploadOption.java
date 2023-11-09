11f1a925b5e8d86c5cba260bfb3126175364eda2
Hamza Iqbal <hamza.cstn@gmail.com>
2023-11-09 21:16:01 +0500

    first six pages

---
+++ app/src/main/java/com/hamzaiqbal/smdproj/uploadOption.java
@@ -0,0 +1,14 @@
+package com.hamzaiqbal.smdproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uploadOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_option);
+    }
+}
---
+++ app/src/main/java/com/hamzaiqbal/smdproj/uploadPhoto.java
@@ -0,0 +1,14 @@
+package com.hamzaiqbal.smdproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
